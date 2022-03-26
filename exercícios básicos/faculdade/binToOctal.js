e7965c60e3d58287c1b06dbdb766cfa8ac20b22c
marcosbarcelos <marcosbarcelosdev@gmail.com>
2022-03-26 19:47:14 -0300

    ex facul

---
+++ exercícios básicos/faculdade/binToOctal.js
@@ -0,0 +1,7 @@
+
+function binarioToOctal(binario) {
+	return parseInt(binario, 2).toString(8);
+}
+
+let binario = 11111011111;
+console.log(binarioToOctal(binario)) //3737
---
+++ exercícios básicos/faculdade/octalToBin.js
@@ -0,0 +1,7 @@
+
+function octalToBinario(octal) {
+	return parseInt(octal, 8).toString(2);
+}
+
+let octal = 144;
+console.log(octalToBinario(octal)) //1100100
