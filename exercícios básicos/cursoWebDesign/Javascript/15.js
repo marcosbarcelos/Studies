aa67a508aca3ecfc4fab5db7afa0629dd3e7a02e
marcosbarcelos <marcosbarcelosdev@gmail.com>
2022-04-19 23:07:06 -0300

    Estudos Javascript

---
+++ exercícios básicos/cursoWebDesign/Javascript/15.js
@@ -0,0 +1,9 @@
+var n = parseFloat(prompt('Digite um número:'));
+
+var i = 1;
+var mensagem = ''
+while(i <= 1000) {
+  mensagem += n + ' x ' + i + ' = ' + (n * i) + '<br>';
+  i++
+}
+document.write(mensagem);
